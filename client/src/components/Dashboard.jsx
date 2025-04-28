e2b2d1fc01dff269ac88eb06a4710f653d5fabe0
imubarak234 <imubarak2424@gmail.com>
2025-04-28 22:52:53 +0100

    Open Conversation Components

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import OpenConversation from "./OpenConversation";
+import { useConversations } from "../contexts/conversationProvider";
 
 const Dashboard = ({ id }) => {
+
+  const { selectedConversation } = useConversations();
   return (
     <div className="d-flex" style={{ height: '100vh' }}>
       <Sidebar id={id} />
+      { selectedConversation && <OpenConversation /> }
     </div>
   );
 }
