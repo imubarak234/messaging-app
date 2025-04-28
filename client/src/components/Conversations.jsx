2497d8e25f58639b9ba09aba54969f073e73999d
imubarak234 <imubarak2424@gmail.com>
2025-04-28 17:29:15 +0100

    Side bar complete

--- client/src/components/Conversations.jsx
+++ client/src/components/Conversations.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { ListGroup } from 'react-bootstrap';
+import { useConversations } from "../contexts/conversationProvider";
 
 
 const Conversations = () => {
 
+  const { conversations, selectConversationIndex } = useConversations();
+
   return (
-    <div>
-      <h3>Conversations</h3>
-    </div>
+    <ListGroup variant="flush">
+      { conversations.map((conversation, index) => (
+        <ListGroup.Item 
+          key={index}
+          action
+          onClick={() => selectConversationIndex(index)}
+          active={conversation.selected}
+        >
+          {conversation.recipients.map(r => r.name).join(", ")}
+        </ListGroup.Item>
+      )) }
+    </ListGroup>
   )
 };
 
